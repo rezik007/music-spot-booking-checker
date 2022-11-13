3e22e555d071d32cb997bb1eeb5fb2c7a20ab573
Patryk Rezler <p.rezler@piwik.pro>
2022-11-13 14:11:54 +0100

    Cleanup of logs and removed mailer

--- index.js
+++ index.js
@@ -1,32 +1,14 @@
 import fetch from 'node-fetch';
 import nodeCron from 'node-cron';
-import Mailgun from 'mailgun.js';
-import formData from 'form-data';
 import dayjs from 'dayjs';
+import notifier from 'node-notifier';
 
 const ROOM_ID = 1;
 const DATE_API_FORMAT = 'YYYY-MM-DD';
 
-const mailer = new Mailgun(formData).client({
-  username: 'rezik',
-  key: process.env.MAILGUN_API_KEY,
-});
-
 let PREVIOUS_NUMBER_OF_RESERVATIONS = 0;
 
 const getSystemUrl = (from, to) => `https://dzwiekowa.pl/system-rezerwacji/api/reservations?from=${from}&to=${to}&scope=active`;
-
-const sendNotification = () => {
-  mailer.messages.create(process.env.MAILGUN_DOMAIN, {
-    from: "Drummer programmer <drummer@programmer.com>",
-    to: ["rezik007@gmail.com"],
-    subject: "SALKA DO WZIĘCIA",
-    text: "Ale będzie grane!",
-    // html: "<h1>SALKA DO WZIĘCIA</h1>"
-  })
-  .then(msg => console.log('Email response data: ', msg))
-  .catch(err => console.log('Unhandled error occured during mail sending: ', err)); // logs any error
-};
 
 const fetchApiData = async () => {
   const fromDate = dayjs().format(DATE_API_FORMAT);
@@ -34,17 +16,31 @@ const fetchApiData = async () => {
   const response = await fetch(getSystemUrl(fromDate, toDate));
   const data = await response.json();
 
-  const singleRoomBookings = data.reservations.filter(booking => booking.roomId = ROOM_ID);
+  const singleRoomBookings = data.reservations.filter(booking => booking.roomId === ROOM_ID);
+
+  console.log('List of bookings: ', singleRoomBookings);
+  console.log('Previous number of bookings: ', PREVIOUS_NUMBER_OF_RESERVATIONS);
+  console.log('Current number of bookings: ', singleRoomBookings.length);
 
   if (singleRoomBookings.length < PREVIOUS_NUMBER_OF_RESERVATIONS) {
-    sendNotification();
+    notifier.notify({
+      title: 'Salka sie zwolniła!',
+      message: 'Czas na próbę ziomuś',
+    });
+  }
+
+  if (singleRoomBookings.length > PREVIOUS_NUMBER_OF_RESERVATIONS) {
+    notifier.notify({
+      title: 'Na salce ktoś zaklepał probę!',
+      message: 'No to peszek',
+    });
   }
 
   PREVIOUS_NUMBER_OF_RESERVATIONS = singleRoomBookings.length;
 }
 
-
 const job = nodeCron.schedule("*/10 * * * *", function jobYouNeedToExecute() {
+  console.log('EXECUTED AT', dayjs().format(`${DATE_API_FORMAT} HH:mm`));
   fetchApiData();
 });
 
